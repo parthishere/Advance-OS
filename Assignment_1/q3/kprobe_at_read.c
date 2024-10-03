3504a3ebfe5d5100d3cc274cf65ff712db83cf03
parthishere <parthishere1234@gmail.com>
2024-10-03 00:45:38 -0600

    working

--- Assignment_1/q3/kprobe_at_read.c
+++ Assignment_1/q3/kprobe_at_read.c
@@ -4,14 +4,15 @@
 #include <linux/kprobes.h>
 #include <linux/kallsyms.h>
 
-static asmlinkage long (*original_sys_read)(unsigned int fd, const char __user *buf, size_t count);
 
-static asmlinkage long jopen_callback(unsigned int fd, const char __user *buf, size_t count){
+// Jprobes is now a deprecated feature. People who are depending on it should
+// migrate to other tracing features or use older kernels. Please consider to
+// migrate your tool to one of the following options:
+
+static int __kprobes pre_handler(struct kprobe *p, struct pt_regs *regs){
 
     printk(KERN_INFO "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
-    printk(KERN_INFO "Debug Info LKM: Callback\n");
-    // Print clone flags
-    printk(KERN_INFO "Clone flags: 0x%lx\n", clone_flags);
+    printk(KERN_INFO "Debug Info LKM: Entry Callback\n");
 
     if (regs) {
         printk(KERN_INFO "Registers:\n");
@@ -30,13 +31,44 @@ static asmlinkage long jopen_callback(unsigned int fd, const char __user *buf, s
     // Print stack (top 16 entries)
     unsigned long * stack = (unsigned long *)regs->sp;
     printk(KERN_INFO "Stack:\n");
-    for (i = 0; i < 16 && !probe_kernel_address(&stack[i], stack[i]); i++) {
+    for (int i = 0; i < 16; i++) {
         printk(KERN_INFO "[%d]: 0x%lx\n", i, stack[i]);
     }
     printk(KERN_INFO "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
 
-    jprobe_return();
+    /* A dump_stack() here will give a stack backtrace */
+	dump_stack();
     return 0;
+
+}
+
+static void __kprobes post_handler(struct kprobe *p, struct pt_regs *regs, unsigned long flags){
+
+	printk(KERN_INFO "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+    printk(KERN_INFO "Debug Info LKM: Return Callback\n");
+ 
+
+    if (regs) {
+        printk(KERN_INFO "Registers:\n");
+        printk(KERN_INFO "RAX: 0x%lx, RBX: 0x%lx, RCX: 0x%lx, RDX: 0x%lx\n",
+               regs->ax, regs->bx, regs->cx, regs->dx);
+        printk(KERN_INFO "RSI: 0x%lx, RDI: 0x%lx, RBP: 0x%lx, RSP: 0x%lx\n",
+               regs->si, regs->di, regs->bp, regs->sp);
+        printk(KERN_INFO "R8:  0x%lx, R9:  0x%lx, R10: 0x%lx, R11: 0x%lx\n",
+               regs->r8, regs->r9, regs->r10, regs->r11);
+        printk(KERN_INFO "R12: 0x%lx, R13: 0x%lx, R14: 0x%lx, R15: 0x%lx\n",
+               regs->r12, regs->r13, regs->r14, regs->r15);
+        printk(KERN_INFO "RIP: 0x%lx, EFLAGS: 0x%lx\n",
+               regs->ip, regs->flags);
+    }
+
+    // Print stack (top 16 entries)
+    unsigned long * stack = (unsigned long *)regs->sp;
+    printk(KERN_INFO "Stack:\n");
+    for (int i = 0; i < 16; i++) {
+        printk(KERN_INFO "[%d]: 0x%lx\n", i, stack[i]);
+    }
+    printk(KERN_INFO "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n");
 
 }
 
@@ -55,41 +87,50 @@ static asmlinkage long jopen_callback(unsigned int fd, const char __user *buf, s
     // struct kprobe 	kp
     // void * 	entry
 
-static struct jprobe my_jprobe = {
-	.entry			= (kprobe_opcode_t *)jopen_callback,
-	.kp = {
-		.symbol_name	= "sys_write",
-	},
+static struct kprobe my_kprobe = {
+	
+	// .symbol_name	= "__x64_sys_write", // for write
+	.symbol_name	= "kernel_clone", // for fork
+	.pre_handler     = pre_handler,
+	.post_handler    = post_handler,
+	
 };
 
-static int __init jprobe_init(void)
+static int __init kprobe_init(void)
 {
     // unsigned long * ksys_write = (unsigned long *)kallsyms_lookup_name("ksys_write");
     int ret;
 
-	ret = register_jprobe(&my_jprobe);
+	ret = register_kprobe(&my_kprobe);
 	if (ret < 0) {
-		printk(KERN_INFO "register_jprobe failed, returned %d\n", ret);
+		printk(KERN_INFO "register_kprobe failed, returned %d\n", ret);
 		return -1;
 	}
-	printk(KERN_INFO "Planted jprobe at %p, handler addr %p\n",
-	       my_jprobe.kp.addr, my_jprobe.entry);
+	printk(KERN_INFO "Planted kprobe at %p, handler addr %p and %p\n",
+	       my_kprobe.addr, my_kprobe.pre_handler, my_kprobe.pre_handler);
 
     return 0;
 }
 
-static void __exit jprobe_exit(void){
-    unregister_jprobe(&my_jprobe);
-    printk(KERN_INFO "jprobe at %p unregistered\n", my_jprobe.kp.addr);
+static void __exit kprobe_exit(void){
+    unregister_kprobe(&my_kprobe);
+    printk(KERN_INFO "Planted kprobe at %p, handler addr %p and %p\n",
+	       my_kprobe.addr, my_kprobe.pre_handler, my_kprobe.pre_handler);
 }
 
-module_init(jprobe_init)
-module_exit(jprobe_exit)
+module_init(kprobe_init)
+module_exit(kprobe_exit)
 
 
 MODULE_AUTHOR("Parth Thakkar");
-MODULE_DESCRIPTION("Rootkit Jprobe");
+MODULE_DESCRIPTION("Rootkit Kprobe");
 MODULE_LICENSE("GPL");
 
 //ffffffff8e8e5400 T ksys_write
 // kallsyms_lookup_name(const char *name) Just 
+
+
+
+
+
+
