e918407b060543710d8021e61d77222a299fda83
parthishere <parthishere1234@gmail.com>
2024-11-07 00:14:09 -0700

    we got segfault

--- Assignment_2/q4/test_progs/test_io.c
+++ Assignment_2/q4/test_progs/test_io.c
@@ -4,7 +4,7 @@
 
 int main() {
     FILE *fp;
-    char buffer[1024 * 1024 * 2]; // 2MB buffer
+    char buffer[1024 * 1024 * 10]; // 10MB buffer
     
     fp = fopen("test.dat", "w");
     if (fp == NULL) {
@@ -12,7 +12,7 @@ int main() {
         return 1;
     }
     
-    // Try to write 2MB (over limit)
+    // Try to write 10MB (over limit)
     fwrite(buffer, 1, sizeof(buffer), fp);
     fclose(fp);
     
